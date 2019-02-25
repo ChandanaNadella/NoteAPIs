3ed907d5224a081510ba2b201a94f29c7f6834f9
Chandana <cnadella@bpktech.com>
2019-02-25 16:31:08 +0530

    UnprocessableEntityObjectResult for errors

---
+++ Note/Note.API.Common/Helpers/UnprocessableEntityObjectResult.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Note.API.Common.Helpers
+{
+    public class UnprocessableEntityObjectResult : ObjectResult
+    {
+        public UnprocessableEntityObjectResult(ModelStateDictionary modelState) : base(new SerializableError(modelState))
+        {
+            if (modelState == null)
+            {
+                //_logger.LogError(string.Format("Date : {0}, Error Status Code: 400, Error Status Message: Bad Request", DateTime.Now.ToString()));
+                //_logger.LogInformation("-----------------------------------------------------------------------------");
+                throw new ArgumentNullException(nameof(modelState));
+            }
+            StatusCode = 422;
+        }
+    }
+}
